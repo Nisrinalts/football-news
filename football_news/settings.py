daa79ab953f699858ffef5e160b5909060b0d344
Nisrina <nisrina.fatimah@ui.ac.id>
2025-09-10 16:24:26 +0700

    tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nisrina-fatimah-footballnews.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "<https://nisrina-fatimah-footballnews.pbp.cs.ui.ac.id>"
+    "https://nisrina-fatimah-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
